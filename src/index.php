90e797f99f15d172e0b07264a682366662b35f6b
MARTIN Jérémy <jeremy.martin@talan.com>
2018-06-15 10:02:40 +0200

    1ère livraison - Avancement 90% - Manque le carrousel de l'étape finale des résultats

--- src/index.php
+++ src/index.php
@@ -19,5 +19,29 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('pages/index/index.twig');
 });
+$app->get('/etape1', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('pages/index/step1.twig');
+});
+$app->get('/etape2', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('pages/index/step2.twig');
+});
+$app->get('/etape3', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('pages/index/step3.twig');
+});
+$app->get('/etape4', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('pages/index/step4.twig');
+});
+$app->get('/etape5', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('pages/index/step5.twig');
+});
+$app->get('/resultats', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('pages/index/resultats.twig');
+});
 
 $app->run();
